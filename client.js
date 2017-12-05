6dbb0b446c1b605dd2af4513d889e7556d714d4a
Brendt Bly <brendt.bly@outlook.com>
2017-12-05 12:05:25 -0600

    added descriptions to projects

---
+++ client.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    // Handler for .ready() called.
+    $('.card').hide();
+    $('.portfolio').on('mouseenter', '.link', function() {
+        console.log('hovered');
+        
+        $(this).parent().parent().parent().find('.card').slideDown('slow', function() {
+            console.log('animated');
+            
+       });
+    });
+    $('.portfolio').on('mouseleave','.item', function() {
+        console.log('hovered');
+        
+        $(this).parent().parent().parent().find('.card').slideUp('slow', function() {
+            console.log('animated');
+            
+       });
+    });
+    // $( "#clickme" ).click(function() {
+    //     $( "#book" ).slideToggle( "slow", function() {
+    //       // Animation complete.
+    //     });
+    //   });
+  });
