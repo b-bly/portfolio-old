9c4613760f8b3c7ff32f04b086cb30f0cd1e095b
Brendt Bly <brendt.bly@outlook.com>
2017-12-12 09:59:46 -0600

    reformatted portfolio links

---
+++ other.js
@@ -0,0 +1,21 @@
+    // $('.portfolio').on('mouseenter', '.link', function() {
+    //     console.log('hovered');
+        
+    //     $(this).parent().parent().parent().find('.card').slideDown('slow', function() {
+    //         console.log('animated');
+            
+    //    });
+    // });
+    // $('.portfolio').on('mouseleave','.item', function() {
+    //     console.log('hovered');
+        
+    //     $(this).parent().parent().parent().find('.card').slideUp('slow', function() {
+    //         console.log('animated');
+            
+    //    });
+    // });
+    // $( "#clickme" ).click(function() {
+    //     $( "#book" ).slideToggle( "slow", function() {
+    //       // Animation complete.
+    //     });
+    //   });
