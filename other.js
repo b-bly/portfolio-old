7afe68c4b791b9f3cd99325f1715aac8d29b6a80
Brendt Bly <brendt.bly@outlook.com>
2018-05-22 19:33:59 -0500

    added two projects

--- other.js
+++ other.js
@@ -1,21 +1,21 @@
-    // $('.portfolio').on('mouseenter', '.link', function() {
-    //     console.log('hovered');
+    $('.portfolio').on('mouseenter', '.link', function() {
+        console.log('hovered');
         
-    //     $(this).parent().parent().parent().find('.card').slideDown('slow', function() {
-    //         console.log('animated');
+        $(this).parent().parent().parent().find('.card').slideDown('slow', function() {
+            console.log('animated');
             
-    //    });
-    // });
-    // $('.portfolio').on('mouseleave','.item', function() {
-    //     console.log('hovered');
+       });
+    });
+    $('.portfolio').on('mouseleave','.item', function() {
+        console.log('hovered');
         
-    //     $(this).parent().parent().parent().find('.card').slideUp('slow', function() {
-    //         console.log('animated');
+        $(this).parent().parent().parent().find('.card').slideUp('slow', function() {
+            console.log('animated');
             
-    //    });
-    // });
-    // $( "#clickme" ).click(function() {
-    //     $( "#book" ).slideToggle( "slow", function() {
-    //       // Animation complete.
-    //     });
-    //   });
+       });
+    });
+    $( "#clickme" ).click(function() {
+        $( "#book" ).slideToggle( "slow", function() {
+          // Animation complete.
+        });
+      });
